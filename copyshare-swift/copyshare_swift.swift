2d1772c2a1e90b95c115eec75dfbc6afbb7ede51
Chloe Surett <chloe@surett.me>
2025-08-12 16:11:17 -0400

    Add login and logout methods

--- copyshare-swift/copyshare_swift.swift
+++ copyshare-swift/copyshare_swift.swift
@@ -21,6 +21,68 @@ public enum CopyShareUploadAccepts: String {
     case length = ""
 }
 
+public enum CopyShareFormActions: String {
+    case login = "login"
+    case logout = "logout"
+    // TODO: Implement other methods from https://github.com/9001/copyparty/blob/hovudstraum/copyparty/httpcli.py#L2490
+}
+
+// from https://theswiftdev.com/easy-multipart-file-upload-for-swift/
+/// Allow Data to have strings appeneded.
+extension Data {
+    mutating func append(
+        _ string: String,
+        encoding: String.Encoding = .utf8
+    ) {
+        guard let data = string.data(using: encoding) else {
+            return
+        }
+        append(data)
+    }
+}
+
+// Modified from https://theswiftdev.com/easy-multipart-file-upload-for-swift/
+public struct CopyPartyMultipartFormRequest {
+    private var boundary: String
+    private var body: Data
+    private let seperator: String = "\r\n"
+    
+    public init(action: CopyShareFormActions) {
+        self.body = .init()
+        self.boundary = UUID().uuidString
+        self.add(key: "act", value: action.rawValue)
+    }
+    
+    private mutating func appendBoundrySeperator() {
+        body.append("--\(boundary)\(seperator)")
+    }
+    
+    private mutating func appendSeperator() {
+        body.append(seperator)
+    }
+    
+    private func disposition(_ key: String) -> String {
+        return "Content-Disposition: form-data; name=\"\(key)\""
+    }
+    
+    public mutating func add(key: String, value: String) {
+        appendBoundrySeperator()
+        body.append(disposition(key) + seperator)
+        appendSeperator()
+        body.append(value + seperator)
+    }
+    
+    public var httpContentTypeHeaderValue: String {
+            "multipart/form-data; boundary=\(boundary)"
+    }
+    
+    public var httpBody: Data {
+        var httpBodyData = body
+        httpBodyData.append("--\(boundary)--\(seperator)")
+        return httpBodyData
+    }
+}
+
 public class CopyShare {
     public var baseURL: URL
     public var session: URLSession
@@ -29,6 +91,55 @@ public class CopyShare {
         self.session = URLSession.init(configuration: .default)
         // Not discretionary by default because user files should be uploaded ASAP.
         self.session.configuration.isDiscretionary = false
+        self.session.configuration.httpShouldSetCookies = true
+    }
+    
+    public func login(username: String?, password: String) async throws -> (String?, Error?) {
+        var request = URLRequest(url: self.baseURL, cachePolicy: .reloadIgnoringLocalCacheData)
+        request.httpMethod = "POST"
+        request.setValue("json", forHTTPHeaderField: "Accept")
+        var formRequest = CopyPartyMultipartFormRequest(action: .login)
+        request.setValue(formRequest.httpContentTypeHeaderValue, forHTTPHeaderField: "Content-Type")
+        if username != nil {
+            formRequest.add(key: "uname", value: username!)
+        }
+        formRequest.add(key: "cppwd", value: password)
+        request.httpBody = formRequest.httpBody
+        print(formRequest.httpContentTypeHeaderValue)
+        print(String(data: request.httpBody!, encoding: .utf8)!)
+        do {
+            let (data, resp) = try await  self.session.data(for: request)
+            guard let resp = resp as? HTTPURLResponse,
+                                (200...299).contains(resp.statusCode)
+            else {
+                print ("server error")
+                return (nil, URLError(.badServerResponse))
+            }
+            return (String(data:data, encoding: .utf8), nil)
+        } catch let error as URLError {
+            return (nil, error)
+        }
+    }
+    
+    public func logout() async throws -> (String?, Error?) {
+        var request = URLRequest(url: self.baseURL, cachePolicy: .reloadIgnoringLocalCacheData)
+        request.httpMethod = "POST"
+        let formRequest = CopyPartyMultipartFormRequest(action: .logout)
+        request.setValue("json", forHTTPHeaderField: "Accept")
+        request.setValue(formRequest.httpContentTypeHeaderValue, forHTTPHeaderField: "Content-Type")
+        request.httpBody = formRequest.httpBody
+        do {
+            let (data, resp) = try await  self.session.data(for: request)
+            guard let resp = resp as? HTTPURLResponse,
+                                (200...299).contains(resp.statusCode)
+            else {
+                print ("server error")
+                return (nil, URLError(.badServerResponse))
+            }
+            return (String(data:data, encoding: .utf8), nil)
+        } catch let error as URLError {
+            return (nil, error)
+        }
     }
     
     public func buildBaseRequest(path: String) throws -> URLRequest {
--- copyshare-swiftTests/copyshare_swiftTests.swift
+++ copyshare-swiftTests/copyshare_swiftTests.swift
@@ -10,10 +10,10 @@ import Testing
 import Foundation
 
 struct copyshare_swiftTests {
+    // TODO: Implement an environment variable to hold the base url.
+    let copyShare = CopyShare.init(baseURL: "https://example.org/")
 
     @Test func put() async {
-        // TODO: Implement an environment variable to hold the base url.
-        let copyShare = CopyShare.init(baseURL: "https://example.org/")
         let tempDirectoryURL = FileManager.default.temporaryDirectory
         let fileName = UUID().uuidString
         let fileURL = tempDirectoryURL.appendingPathComponent(fileName)
@@ -31,6 +31,28 @@ struct copyshare_swiftTests {
             print(result ?? "None")
         } catch {
             assertionFailure("Upload failed")
+        }
+    }
+    
+    @Test func login() async {
+        do {
+            let (_, error) = try await copyShare.login(username: "test", password: "test")
+            assert(error == nil, "Failed to login error \(String(describing: error))")
+            // Print out the cookies to visually check if it was set
+            assert(HTTPCookieStorage.shared.cookies?.count ?? 0 > 0, "Cookie was not set")
+        } catch {
+            assertionFailure("Login failed")
+        }
+    }
+    
+    @Test func logout() async {
+        await login()
+        do {
+            let (_, error) = try await copyShare.logout()
+            assert(error == nil, "Failed to logout error \(String(describing: error))")
+            assert(HTTPCookieStorage.shared.cookies?.count ?? 0 == 0, "Cookie was not unset")
+        } catch {
+            assertionFailure("Logout failed")
         }
     }
 }
