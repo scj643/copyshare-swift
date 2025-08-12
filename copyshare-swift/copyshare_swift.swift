681551d1a828c99245db21ee1b495ea066b5c3b3
Chloe Surett <chloe@surett.me>
2025-08-12 14:24:47 -0400

    Expose options for putFile and break out request building

--- copyshare-swift/copyshare_swift.swift
+++ copyshare-swift/copyshare_swift.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+public enum CopyShareChecksums: String {
+    case no = ""
+    case md5 = "md5"
+    case sha256 = "sha256"
+    case b2 = "b2"
+    case b2s = "b2s"
+}
+
+public enum CopyShareUploadAccepts: String {
+    case json = "json"
+    case url = "url"
+    case length = ""
+}
+
 public class CopyShare {
     public var baseURL: URL
     public var session: URLSession
@@ -17,18 +31,42 @@ public class CopyShare {
         self.session.configuration.isDiscretionary = false
     }
     
-    /// Upload a file using the PUT HTTP method.
-    ///
-    /// Returns the response JSON as text or an error if it has failed to upload.
-    public func putFile(localPath: URL, remotePath: String) async throws -> (String?, Error?)
-    {
-        guard let requestURL = URLComponents(url: URL(string: remotePath, relativeTo: self.baseURL)!, resolvingAgainstBaseURL: true) else {
+    public func buildBaseRequest(path: String) throws -> URLRequest {
+        guard let requestURL = URLComponents(url: URL(string: path, relativeTo: self.baseURL)!, resolvingAgainstBaseURL: true) else {
             throw URLError(.badURL)
         }
-        var request = URLRequest(url: requestURL.url!)
+        return URLRequest(url: requestURL.url!)
+    }
+    
+    // TODO: Add upload compression option
+    public func buildUploadRequest(path: String, accepts: CopyShareUploadAccepts = .json, rand: Int? = nil, checksum: CopyShareChecksums? = nil, lifetime: UInt128? = nil) throws -> URLRequest {
+        var request = try buildBaseRequest(path: path)
+        request.setValue(accepts.rawValue, forHTTPHeaderField: "Accept")
+        if rand != nil {
+            // Request a random file name for upload.
+            request.setValue(String(rand!), forHTTPHeaderField: "Rand")
+        }
+        if checksum != nil {
+            request.setValue(checksum?.rawValue, forHTTPHeaderField: "CK")
+        }
+        if lifetime != nil {
+            request.setValue(String(lifetime!), forHTTPHeaderField: "Life")
+        }
+        return request
+    }
+    
+    /// Upload a file using the PUT HTTP method.
+    ///
+    /// see buildUploadRequest for more info on arguments.
+    public func putFile(localPath: URL, remotePath: String, accepts: CopyShareUploadAccepts = .json, rand: Int? = nil, checksum: CopyShareChecksums? = nil, lifetime: UInt128? = nil) async throws -> (String?, Error?)
+    {
+        var request: URLRequest
+        do {
+            request = try buildUploadRequest(path: remotePath, accepts: accepts, rand: rand, checksum: checksum, lifetime: lifetime)
+        } catch {
+            return (nil, error)
+        }
         request.httpMethod = "PUT"
-        // Return a JSON response
-        request.setValue("json", forHTTPHeaderField: "Accept")
         do {
             let (data, resp) = try await self.session.upload(for: request, fromFile: localPath)
             guard let resp = resp as? HTTPURLResponse,
